df0049bbd9e889d9abc8d5ddac1eff32df640e7e
thrmadhkdehddjs <H098S02621@chunchon.gwe.hs.kr>
2025-11-21 20:19:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ h1 {
 """, unsafe_allow_html=True)
 
 # --- 3. 모델 로드 ---
-file_id = '19dS6rAzHlGekODz1l2F020D9XMlhNDYS'
+file_id = '1o0YtuYGUmVBW1QwGuXG51aByYXxsmqHC'
 model_path = 'model.pkl'
 
 @st.cache_resource
